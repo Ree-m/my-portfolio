5364cb4f9e1f66bd4435a28d13a9b906f888ed69
Ree-m <reembsrat@gmail.com>
2023-06-24 19:18:07 +0100

    added components and typewriter-effect

---
+++ app/Components/Home/Home.jsx
@@ -0,0 +1,22 @@
+
+const Home = () => {
+  return (
+    <div>
+         <h1>
+                Hi There!{" "}
+                <span>
+                  👋🏻
+                </span>
+
+                </h1>
+
+                <h1>
+                I AM
+                <strong>REEM BSRAT</strong>
+              </h1>
+
+    </div>
+  )
+}
+
+export default Home
