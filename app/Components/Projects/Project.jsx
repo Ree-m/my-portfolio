a7854abfdd6a1f76f58f15b8d3926a9a04d31e33
Ree-m <reembsrat@gmail.com>
2023-07-16 23:48:04 +0300

    changed project link to router

--- app/Components/Projects/Project.jsx
+++ app/Components/Projects/Project.jsx
@@ -3,17 +3,20 @@ import Image from "next/image";
 // import ImageSrc from "/Assests/skinology-ecommerce-app-client-onrender-com-1024x768desktop-4d90e6.jpg"
 import Link from "next/link";
 import styles from "../../styles/project.module.css";
+import { useRouter } from "next/router";
 
 import { AiFillGithub } from "react-icons/ai";
 import { CgWebsite } from "react-icons/cg";
 import { BiDetail } from "react-icons/bi";
 const Project = ({ name, description, ImageSrc, id, githubLink, demoLink }) => {
+  const router =useRouter()
+
   const openInNewTab = (url) => {
     window.open(url, "_blank", "noreferrer");
   };
   return (
-    <Link href={`/project/${id}`}>
-    <div className={styles.project} >
+    // <Link href={`/project/${id}`}>
+    <div className={styles.project} onClick={()=>router.push(`/project/${id}`)} >
       <div className={styles.imgContainer}>
         {/* <Image
             src={ImageSrc}
@@ -36,30 +39,24 @@ const Project = ({ name, description, ImageSrc, id, githubLink, demoLink }) => {
         <h3>{name}</h3>
         <p>{description}</p>
         <div className={styles.btns}>
-          <Link href={`/project/${id}`}>
-            <button>
+            <button onClick={()=> router.push(`/project/${id}`)}>
               <BiDetail />
               Details
             </button>
-          </Link>
 
-          <Link href={``}>
             <button onClick={() => openInNewTab(githubLink)}>
               <AiFillGithub />
               Github
             </button>
-          </Link>
 
-          <Link href={``}>
             <button onClick={() => openInNewTab(demoLink)}>
               <CgWebsite />
               Demo
             </button>
-          </Link>
         </div>
       </div>
     </div>
-    </Link>
+    // </Link>
   );
 };
 
