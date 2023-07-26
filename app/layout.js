c03a53340e3bf6568c8e75ab9e1967a5b457031c
Ree-m <reembsrat@gmail.com>
2023-07-26 12:31:14 +0300

    favicon

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import { Raleway } from "next/font/google";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 
-const raleway=Raleway({subsets:["latin"]})
+const raleway = Raleway({ subsets: ["latin"] });
 
 export const metadata = {
   title: "reembsrat",
@@ -13,6 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+
       <body className={raleway.className}>
         <Header />
         {children}
